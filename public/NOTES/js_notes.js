08cafe0d6b3707d2e2aa4a9becf38e704fe4beae
Thomas Allen <thomasallen@Thomass-MacBook-Pro.local>
2015-10-02 10:33:32 -0500

    Added inline and external html files along with external js file.

---
+++ public/NOTES/js_notes.js
@@ -0,0 +1,99 @@
+// Link to html
+<script src="js/external.js"></script>
+
+// read evaluate print loop:
+repl;
+
+// declare variable:
+var "hello";
+
+// undefined
+unassigned varilable without value or type
+
+// to find out type of variable
+typeof;
+
+// boolean has 2 values(on or off)
+1 or 0;
+
+// logical AND
+&&;
+
+// logical or
+||;
+
+// logical NOT
+!;
+
+// read from right to left for booleans like this:
+!!boolean;
+
+// only false number is 0
+// any content is true
+
+// modulo gives remainder when doing division
+7 % 3 = 1;
+
+// increment
+++
+
+// decrement
+--
+
+// increments of 2(example)
+x += 2;
+
+// use .tofixed() to avoid floating numbers
+.tofixed();
+
+// number you pass in the parenthesis is the number it rounds up to
+
+// multi-line strings
+var multiLine = "Hello \
+World!";
+
+// Special Characters and Escaping
+\n	New line
+\t	Tab
+\\	Backslash
+\'	Apostrophe or single quote
+\"	Double quote
+
+// string concatenation
+var sayHello = "Hello " + "World";
+
+// Parsing a String into a Number
+parseInt("1") + 2; // yields number 3
+
+
+// functions
+
+.length : property that describes the number of characters in the string.
+
+.indexOf(char) : returns the index of the specified character in the string.
+
+.replace(find, replace) : returns a copy of the string after performing a substitution.
+
+.substring(fromIndex, toIndex) : returns a subset of the original string based on the provided indices.
+
+.toUpperCase() : returns a copy of the string in all upper case.
+
+.toLowerCase() : returns a copy of the string in all lower case.
+
+.trim() : returns a copy of the string with whitespace at the beginning and end removed.
+
+String.fromCharCode(code) : returns a string based on the provided ASCII code.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
