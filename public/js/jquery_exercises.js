e084e3b6906c95fb3c5a7e4082c2326d2a676068
Thomas Allen <thomasallen@Thomass-MacBook-Pro.local>
2015-10-15 10:13:02 -0500

    PRACTICE WITH ID SELECTORS IN JQUERY EXERCISE.

--- public/js/jquery_exercises.js
+++ public/js/jquery_exercises.js
@@ -1,3 +1,8 @@
 $(document).ready(function() {
     alert( 'The DOM has finished loading!' );
 });
+
+$(document).ready(function() {
+            var contents = $('#para1').html();
+            alert(contents);
+});
