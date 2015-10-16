350bcb35e6107b6b99f123aed6c683ef6829d2c3
Thomas Allen <thomasallen@Thomass-MacBook-Pro.local>
2015-10-16 13:53:07 -0500

    NEW JQUERY CLICK/EACH FUNCTIONS AND NOTES.

--- public/js/jquery_faq.js
+++ public/js/jquery_faq.js
@@ -4,3 +4,13 @@ $(document).ready(function() {
         $('.invisible').toggleClass('visible');
     });
 });
+
+$(document).ready(function() {
+	$('ul').each(function(){
+		$(this).children('li').first().css('font-weight', 'bold');
+	});
+	$('li').click(function(event) {
+        event.preventDefault();
+        $(this).parent().toggleClass('invisible');
+    });
+});
