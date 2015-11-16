14ba2d4c00a197bb5ac07d0057bc5dd32b30bd8c
Thomas Allen <thomasallen@Thomass-MacBook-Pro.local>
2015-11-16 09:09:16 -0600

    New notes.

--- public/NOTES/php_notes.php
+++ public/NOTES/php_notes.php
@@ -345,25 +345,7 @@ var_dump($_POST);
     </form>
 </body>
 </html>
-<!-- AND ALSO -->
-<?php
-var_dump($_POST);
-?>
-<!DOCTYPE html>
-<html>
-<head>
-    <title>POST Example</title>
-</head>
-<body>
-    <form method="POST">
-        <label>Name</label>
-        <input type="text" name="name"><br>
-        <label>Number</label>
-        <input type="text" name="number"><br>
-        <input type="submit">
-    </form>
-</body>
-</html>
+
 
 
 
