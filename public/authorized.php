fdaaa24ababc75150464468b3d467069afb64953
Thomas Allen <thomasallen@Thomass-MacBook-Pro.local>
2015-11-16 15:01:24 -0600

    Additional notes on php sessions. Completed authorized/login/logout exercises.

--- public/authorized.php
+++ public/authorized.php
@@ -1,3 +1,11 @@
+<?php
+session_start();
+    if(!isset($_SESSION['LOGGED_IN_USER'])) {
+        header('Location: login.php');
+        die();
+    }
+    $name = $_SESSION['LOGGED_IN_USER'];
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -5,5 +13,7 @@
 </head>
 <body>
     <h1>Authorized</h1>
+    <h2>Your logged in username is: <?= $name ?></h2>
+    <a href="logout.php">LOGOUT</a>
 </body>
 </html>
--- public/login.php
+++ public/login.php
@@ -1,19 +1,23 @@
 <?php
-var_dump($_POST);
+session_start();
 function pageController()
 {
+	$sessionId = session_id();
 	$name = isset($_POST['name']) ? strip_tags(htmlspecialchars($_POST['name'])) : "";
 	$password = isset($_POST['password']) ? strip_tags(htmlspecialchars($_POST['password'])) : "";
-
-	echo $name . " " . $password;
-
+	$javascript = '';
 	if($name == "guest" && $password == "password"){
-        header('Location:authorized.php');
+        $_SESSION['LOGGED_IN_USER'] = $name;
+        header('Location: authorized.php');
         die();
-    };
+    }else if ($name != "" || $password != ""){
+		$javascript = 'alert("Incorrect input.")';
+    }
 	return array(
 		'name' 	 => $name,
-		'password' => $password
+		'password' => $password,
+		'sessionId' => $sessionId,
+		'javascript' => $javascript
 	);
 };
 extract(pageController());	
@@ -24,12 +28,18 @@ extract(pageController());
     <title>Login</title>
 </head>
 <body>
+	Session Id: <?= $sessionId; ?><br>
     <form method="POST">
         <label>Name</label>
         <input value ="<?= $name ?>"type="text" name="name"><br>
         <label>Password</label>
-        <input type="text" name="password"><br>
+        <input type="password" name="password"><br>
         <input type="submit">
+        <input type="hidden" name="reset" value="reset">
+        <input type="submit" value="Reset Counter">
     </form>
+    <script language="javascript">
+    	<?= $javascript; ?>
+	</script>
 </body>
 </html>
