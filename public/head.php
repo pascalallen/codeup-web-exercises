63be8f568cff632baaa79e7c97e3d06cc4488221
Thomas Allen <thomasallen@Thomass-MacBook-Pro.local>
2015-11-17 10:35:45 -0600

    Created small template website with seperate template files that are added using php require.

---
+++ public/head.php
@@ -0,0 +1,12 @@
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Small Website</title>
+	<!-- BOOTSTRAP -->
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<!-- CUSTOM CSS -->
+	<link rel="stylesheet" type="text/css" href="css/small_website.css">
+	<!-- TITLE IMG -->
+	<!-- <link rel="shortcut icon" href="img/mole.png"> -->
+</head>
---
+++ public/small_website_index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<?php require "head.php"; ?>
+<body>
+	<?php require "navbar.php"; ?>
+	<div class="site-wrapper">
+		<h1 class="header">Small Templated Website</h1>
+		<div></div>
+		<div></div>
+		<div></div>	
+	</div>
+	<?php require "footer.php"; ?>
+	<!-- JQUERY -->
+	<script src="js/jquery-2.1.4.min.js"></script>
+	<!-- CUSTOM JAVASCRIPT -->
+	<script src="js/small_website.js"></script>
+</body>
+</html>
