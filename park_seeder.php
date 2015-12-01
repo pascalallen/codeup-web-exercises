6516bcd9350f8dca0b2750d11955b715c9e279d1
Thomas Allen <thomasallen@Thomass-MacBook-Pro.local>
2015-12-01 15:51:30 -0600

    Added parks_logins file to be required by park_migration and park_seeder and included parks_logins to .gitignore file so it is not committed.

--- park_seeder.php
+++ park_seeder.php
@@ -1,8 +1,5 @@
 <?php
-define('DB_HOST', '127.0.0.1');
-define('DB_NAME', 'parks_db');
-define('DB_USER', 'parks_user');
-define('DB_PASS', 'ihave555');
+require_once 'parks_logins';
 
 require 'db_connect.php';
 
