751b1772bc063e60620843e88a1a56b9b1efe238
Thomas Allen <thomasallen@Thomass-MacBook-Pro.local>
2015-12-02 14:04:49 -0600

    Added styling to query and results exercise.

--- public/national_parks.php
+++ public/national_parks.php
@@ -23,38 +23,42 @@
 	<link rel="stylesheet" type="text/css" href="css/national_parks.css">
 	<!-- TITLE IMG -->
 	<link rel="shortcut icon" href="img/codeup-arrow.png">
+	<!-- CUSTOM FONT -->
+	<link href='https://fonts.googleapis.com/css?family=Rock+Salt' rel='stylesheet' type='text/css'>
 </head>
 <body>
-	<div class="container">
-		<h1><?= $page; ?></h1>
-		<h2>National Parks</h2>
-		<h3>Database Driven Web Application</h3>
-		<table class="table">
-			<thead>
-				<tr>
-				<th>Name</th>
-				<th>Location</th>
-				<th>Established</th>
-				<th>Acres</th>
-				</tr>
-		    </thead>
-			<?php foreach ($parks as $park) : ?>
-				<tbody>
-					<tr class="success">
-					    <td><?= $park['name'] ?> </td>
-					    <td><?= $park['location'] ?> </td>
-					    <td><?= $park['date_established'] ?> </td>
-					    <td><?= $park['area_in_acres'] ?> </td>
+	<div class="site-wrapper">
+		<div class="container">
+			<!-- <h1><?= $page; ?></h1> -->
+			<h2>National Parks</h2>
+			<!-- <h3>Database Driven Web Application</h3> -->
+			<table class="table">
+				<thead>
+					<tr>
+					<th>Name</th>
+					<th>Location</th>
+					<th>Established</th>
+					<th>Acres</th>
 					</tr>
-				</tbody>
-			<?php endforeach; ?>
-		</table>
-		<?php if ($page != 1) : ?>
-			<a href="?page=<?= ($page - 1) ?>">Previous</a>
-		<?php endif; ?>
-		<?php if ($page != 5) : ?>
-			<a href="?page=<?= ($page + 1) ?>">Next</a>
-		<?php endif; ?>
+			    </thead>
+				<?php foreach ($parks as $park) : ?>
+					<tbody>
+						<tr class="table-striped">
+						    <td><?= $park['name'] ?> </td>
+						    <td><?= $park['location'] ?> </td>
+						    <td><?= $park['date_established'] ?> </td>
+						    <td><?= $park['area_in_acres'] ?> </td>
+						</tr>
+					</tbody>
+				<?php endforeach; ?>
+			</table>
+			<?php if ($page != 1) : ?>
+				<a href="?page=<?= ($page - 1) ?>">Previous</a>
+			<?php endif; ?>
+			<?php if ($page != 5) : ?>
+				<a href="?page=<?= ($page + 1) ?>">Next</a>
+			<?php endif; ?>
+		</div>
 	</div>
 </body>
 </html>
