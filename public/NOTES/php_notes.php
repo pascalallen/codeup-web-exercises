25f199278950c1c850961ee6edee55baa9a9ba22
Thomas Allen <thomasallen@Thomass-MacBook-Pro.local>
2015-12-07 11:14:31 -0600

    Added notes about intro to magic methods. __get, __set.

--- public/NOTES/php_notes.php
+++ public/NOTES/php_notes.php
@@ -556,7 +556,30 @@ class Person
     }
 }
 
+// INTRO TO MAGIC METHODS
+class Datastore
+{
+    // Array to store our key/value data
+    private $data = [];
 
+    // Magic setter to populate $data array
+    public function __set($name, $value)
+    {
+        // Set the $name key to hold $value in $data
+        $this->data[$name] = $value;
+    }
+
+    // Magic getter to retrieve values from $data
+    public function __get($name)
+    {
+        // Check for existence of array key $name
+        if (array_key_exists($name, $this->data)) {
+            return $this->data[$name];
+        }
+
+        return null;
+    }
+}
 
 
 
